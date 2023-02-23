95ee47d464c14d41dce81ad9917ff9149d42dbf3
DanisPC\Daniel <dani_baur@yahoo.de>
2023-02-23 11:36:16 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tileProvider = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const  mapOptions = {
-    center: [51.505, -0.09],
+    center: [48.1844, 11.4668],
     zoom: 10,
     pmIgnore: false
     }
@@ -15,3 +15,23 @@ map.pm.addControls({
     position: 'topleft',  
     drawPolyline: false,  
   });  
+
+
+  
+
+function WriteShapefiles() {
+    var layers = L.PM.Utils.findLayers(map);
+    if(layers.length != 0) {
+        var group = L.featureGroup();
+        layers.forEach((layer)=>{
+            group.addLayer(layer);
+        });
+        shapes = group.toGeoJSON();
+        /*
+        shapes.features.forEach((shape)=>{
+            console.log(shape);
+        });
+        */
+        console.log(shapes);
+    }
+}
