9da1b4d940ce6bbfad1a3a01d5e00087356b2d14
DanisPC\Daniel <dani_baur@yahoo.de>
2023-04-11 17:21:23 +0200

    added Syngrid as submodule

--- IDP_MapTool_Flask/maptool/network_editor/routes.py
+++ IDP_MapTool_Flask/maptool/network_editor/routes.py
@@ -19,8 +19,8 @@ def networks():
 def editableNetwork():
     #on opening of the network view the js code requests full information of the previously selected network
     if request.method == 'GET':
-        plz = session.get('plz')['key']
-        kcid_bcid = session.get('kcid_bcid')['key']
+        plz = session.get('plz')['value']
+        kcid_bcid = session.get('kcid_bcid')['value']
         gg = GridGenerator(plz=plz)
         pg = gg.pgr
         testnet = pg.read_net(plz=plz, kcid=kcid_bcid[0], bcid=kcid_bcid[1])
